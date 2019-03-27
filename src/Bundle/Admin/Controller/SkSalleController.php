ac899b2b725f0791640a95606a91028d88de7983
julkwel <julienrajerison5@gmail.com>
2019-03-27 13:19:02 +0300

    [SALLE] CRUD SALLE

---
+++ src/Bundle/Admin/Controller/SkSalleController.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julkwel
+ * Date: 3/27/19
+ * Time: 12:07 PM
+ */
+
+namespace App\Bundle\Admin\Controller;
+
+
+use App\Shared\Entity\SkSalle;
+use App\Shared\Form\SkSalleType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SkSalleController extends Controller
+{
+    /**
+     * @return \App\Shared\Repository\SkEntityManager|object
+     */
+    public function getEntityService()
+    {
+        return $this->get('sk.repository.entity');
+    }
+
+    public function getUserConnected()
+    {
+        return $this->get('security.token_storage')->getToken()->getUser();
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     */
+    public function indexAction()
+    {
+        $_salle_list = $this->getEntityService()->getAllListByEts(SkSalle::class);
+        return $this->render('AdminBundle:SkSalle:index.html.twig', array(
+            'salle_list' => $_salle_list
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Exception
+     */
+    public function newAction(Request $request)
+    {
+        $_user_connected = $this->getUserConnected()->getEtsNom();
+
+        $_salle = new SkSalle();
+        $_form = $this->createForm(SkSalleType::class, $_salle);
+        $_form->handleRequest($request);
+
+        if ($_form->isSubmitted() && $_form->isValid()) {
+            $_salle->setEtsNom($_user_connected);
+            $this->getEntityService()->saveEntity($_salle, 'new');
+            try {
+                $this->getEntityService()->setFlash('success', 'salle ajouter avec success');
+            } catch (\Exception $exception) {
+                $this->getEntityService()->setFlash('error', 'Un erreur se produite');
+                $exception->getMessage();
+            }
+
+            return $this->redirectToRoute('salle_index');
+        }
+
+        return $this->render('AdminBundle:SkSalle:add.html.twig', array(
+            'form' => $_form->createView(),
+            'salle' => $_salle
+        ));
+    }
+
+    /**
+     * @param Request $request
+     * @param SkSalle $salle
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Exception
+     */
+    public function updateAction(Request $request, SkSalle $salle)
+    {
+        $_form = $this->createForm(SkSalleType::class, $salle);
+        $_form->handleRequest($request);
+
+        if ($_form->isSubmitted() && $_form->isValid()) {
+            $_add_salle = $this->getEntityService()->saveEntity($salle, 'update');
+            if ($_add_salle === true) {
+                try {
+                    $this->getEntityService()->setFlash('success', 'modification salle avec success');
+                } catch (\Exception $exception) {
+                    $this->getEntityService()->setFlash('success', 'modification salle avec success');
+                    $exception->getMessage();
+                }
+            }
+
+            return $this->redirectToRoute('salle_index');
+        }
+
+        return $this->render('AdminBundle:SkSalle:edit.html.twig', array(
+            'form' => $_form->createView(),
+            'salle' => $salle
+        ));
+    }
+
+    /**
+     * @param SkSalle $skSalle
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     * @throws \Exception
+     */
+    public function deleteAction(SkSalle $skSalle)
+    {
+        $_delete_salle = $this->getEntityService()->deleteEntity($skSalle, '');
+        if ($_delete_salle === true) {
+            try {
+                $this->getEntityService()->setFlash('success', 'suppression salle réussie');
+            } catch (\Exception $exception) {
+                $this->getEntityService()->setFlash('error', 'un erreur se produite');
+                $exception->getMessage();
+            }
+
+            return $this->redirectToRoute('salle_index');
+        }
+    }
+
+    public function reservationAction(Request $request,SkSalle $skSalle)
+    {
+        $_form = $this->createForm(SkSalleType::class,$skSalle);
+        $_form->handleRequest($request);
+
+        if ($_form->isSubmitted() && $_form->isValid()) {
+            $debut_reservation = $request->request->get('debut');
+            $fin_reservation = $request->request->get('fin');
+            $motif_reservation = $request->request->get('motif');
+            $skSalle->setIsReserve(true);
+            $skSalle->setDebReserve(new \DateTime($debut_reservation));
+            $skSalle->setFinReserve(new \DateTime($fin_reservation));
+            $skSalle->setMotifs($motif_reservation);
+            try {
+                $this->getEntityService()->saveEntity($skSalle,'update');
+                $this->getEntityService()->setFlash('success','reservation ajouté');
+            } catch (\Exception $exception){
+                $exception->getMessage();
+            }
+
+            return $this->redirectToRoute('salle_index');
+        }
+
+        return $this->render('@Admin/SkSalle/reservation.html.twig',array(
+            'form' => $_form->createView(),
+            'salle' => $skSalle
+        ));
+    }
+
+    /**
+     * @param SkSalle $skSalle
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function annuleAction(SkSalle $skSalle)
+    {
+        try {
+            $skSalle->setIsReserve(false);
+            $skSalle->setMotifs(null);
+            $this->getEntityService()->saveEntity($skSalle,'update');
+        } catch (\Exception $exception) {
+            $exception->getMessage();
+        }
+
+        return $this->redirectToRoute('salle_index');
+    }
+}
---
+++ src/Shared/Form/SkSalleType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julkwel
+ * Date: 3/27/19
+ * Time: 11:56 AM
+ */
+
+namespace App\Shared\Form;
+
+
+use App\Shared\Entity\SkSalle;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SkSalleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('salleNom',TextType::class,array(
+                'label'=>'Salle nom'
+            ))
+            ->add('salleNumero',TextType::class,array(
+                'label'=>'Numero salle'
+            ))
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'=>SkSalle::class
+        ));
+    }
+}
