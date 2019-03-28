34030f8a1e1b284c457c5baddca3fe4ace534697
julkwel <julienrajerison5@gmail.com>
2019-03-28 11:19:22 +0300

    [EMPLOI DU TEMPS] ADD CALENDAR

--- src/Bundle/Admin/Controller/SkEdtController.php
+++ src/Bundle/Admin/Controller/SkEdtController.php
@@ -10,24 +10,86 @@ namespace App\Bundle\Admin\Controller;
 
 
 use App\Shared\Entity\SkClasse;
+use App\Shared\Entity\SkEdt;
+use App\Shared\Entity\SkMatiere;
+use App\Shared\Form\SkEdtType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class SkEdtController extends Controller
 {
+    public function getUserConnected()
+    {
+        return $this->get('security.token_storage')->getToken()->getUser();
+    }
+
+    public function getEntityService()
+    {
+        return $this->get('sk.repository.entity');
+    }
+
     /**
      * @param SkClasse $skClasse
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction(SkClasse $skClasse)
     {
-        dump($skClasse);
-        die();
-        return $this->render('@Admin/SkClasse/edt.html.twig', array());
+        $_edt = $this->getDoctrine()->getRepository(SkEdt::class)->findBy(array('edtClasse' => $skClasse));
+//        dump($_edt);die();
+        return $this->render('@Admin/SkClasse/edt.html.twig', array(
+            'classe' => $skClasse,
+            'edt' => $_edt
+        ));
     }
 
+    /**
+     * @param Request $request
+     * @param SkClasse $skClasse
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function addEdtAction(Request $request, SkClasse $skClasse)
     {
 
+        try {
+            $_edt = new SkEdt();
+            $_user_ets = $this->getUserConnected()->getEtsNom();
+            $_form = $this->createForm(SkEdtType::class, $_edt);
+            $_form->handleRequest($request);
+            $_mat_list = $this->getDoctrine()->getRepository(SkMatiere::class)->findBy(array('matClasse' => $skClasse));
+
+
+            if ($_form->isSubmitted() && $_form->isValid()) {
+                try {
+                    $_mat = $request->request->get('matiere');
+                    $_date_debut = $request->request->get('debut');
+                    $_date_fin = $request->request->get('fin');
+                    $_mat = $this->getDoctrine()->getRepository(SkMatiere::class)->find($_mat);
+                    $_edt->setEtsNom($_user_ets);
+                    $_edt->setMatNom($_mat);
+                    $_edt->setEdtClasse($skClasse);
+                    $_edt->setEtdDateDeb(new \DateTime($_date_debut));
+                    $_edt->setEtdDateFin(new \DateTime($_date_fin));
+                    try {
+                        $this->getEntityService()->saveEntity($_edt, 'new');
+                    } catch (\Exception $exception) {
+                        $exception->getMessage();
+                    }
+                    $this->getEntityService()->setFlash('success', 'Emploi du temps ajouté avec success');
+                } catch (\Exception $exception) {
+                    $exception->getMessage();
+                }
+                return $this->redirect($this->generateUrl('classe_edt', array('id' => $skClasse->getId())));
+            }
+        } catch (\Exception $exception) {
+            $exception->getMessage();
+        }
+
+
+        return $this->render('@Admin/SkClasse/template.html.twig', array(
+            'matieres' => $_mat_list,
+            'edt' => $_edt,
+            'classe' => $skClasse,
+            'form' => $_form->createView()
+        ));
     }
 }
