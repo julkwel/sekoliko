3d80e238dc88096d7f9270a7ecdac424d7904f5f
vonyms <vonirandria07@gmail.com>
2019-03-28 22:45:20 +0300

    Rectification couleur bouton

--- public/js/traduction.js
+++ public/js/traduction.js
@@ -3,4 +3,11 @@
  */
 $(document).ready(function(){    
     $("input[placeholder='Search for...']").attr("placeholder","Tapez ici votre recherche");
+    $("label[for='form_prenom']").text("Prénom");
+    $("label[for='form_username']").text("Nom d' utilisateur");
+    $("a[title='Modifier']").css({
+        "background-color":"#41c120",
+        "border-color":"#41c120"
+    });
+    $("a[title='Modifier']").removeAttr("style");
 });
