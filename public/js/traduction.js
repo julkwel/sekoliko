2a6e135ce0f5445963b30c5a976631d8c9bf89d3
vonyms <vonirandria07@gmail.com>
2019-03-29 13:06:01 +0300

    supprimer les codes non utilises de traduction.js

--- public/js/traduction.js
+++ public/js/traduction.js
@@ -2,12 +2,6 @@
  * Traduction et correction des textes
  */
 $(document).ready(function(){    
-    $("input[placeholder='Search for...']").attr("placeholder","Tapez ici votre recherche");
     $("label[for='form_prenom']").text("Prénom");
     $("label[for='form_username']").text("Nom d' utilisateur");
-    $("a[title='Modifier']").css({
-        "background-color":"#41c120",
-        "border-color":"#41c120"
-    });
-    $("a[title='Modifier']").removeAttr("style");
 });
