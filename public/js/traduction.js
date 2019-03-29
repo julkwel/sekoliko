7897077bd4452c02dda322b011361634f48c06a9
vonyms <vonirandria07@gmail.com>
2019-03-29 12:13:33 +0300

    couleur btn edit

--- public/js/traduction.js
+++ public/js/traduction.js
@@ -2,5 +2,8 @@
  * Traduction et correction des textes
  */
 $(document).ready(function(){    
-    $("input[placeholder='Search for...']").attr("placeholder","Tapez ici votre recherche");
+    $("a[title='Modifier']").css({
+        "background-color":"#41c120",
+        "border-color":"#41c120"
+    });
 });
