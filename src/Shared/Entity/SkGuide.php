51fd99c2327c460d9d60adc8e0bc25f7ac89c1c9
julkwel <julienrajerison5@gmail.com>
2019-03-29 15:56:23 +0300

    [GUIDE] GLOBAL USER GUIDE

---
+++ src/Shared/Entity/SkGuide.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julkwel
+ * Date: 3/29/19
+ * Time: 2:10 PM
+ */
+
+namespace App\Shared\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SkGuide.
+ *
+ * @ORM\Table(name="sk_guide")
+ * @ORM\Entity
+ */
+class SkGuide
+{
+    use SkEtablissement;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="desciption", type="string",length=200, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    
+    
+}
---
+++ src/Shared/Form/SkGuideType.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julkwel
+ * Date: 3/29/19
+ * Time: 3:04 PM
+ */
+
+namespace App\Shared\Form;
+
+
+use App\Shared\Entity\SkGuide;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SkGuideType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('description',TextType::class,array(
+                'label'=>'description'
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'=>SkGuide::class
+        ));
+    }
+}
