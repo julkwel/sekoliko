f0ffd7988605b07137eb771d443c2085c5d682f5
TolotraSmile <tolotra.raharison@gmail.com>
2019-04-06 22:50:22 +0200

    Setup front page with React & Encore

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="row">
+      <h1>Hello Sekoliko (front page from React)</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,23 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
-    .cleanupOutputBeforeBuild()
-    .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+  // the project directory where compiled assets will be stored
+  .setOutputPath('public/build/')
+  // the public path used by the web server to access the previous directory
+  .setPublicPath('/build')
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  // uncomment to create hashed filenames (e.g. app.abc123.css)
+  // .enableVersioning(Encore.isProduction())
+  .enableReactPreset()
+  // uncomment to define the assets of the project
+  .addEntry('js/app', './assets/js/app.js');
+// .addStyleEntry('css/app', './assets/css/app.scss')
 
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+// uncomment if you use Sass/SCSS files
+// .enableSassLoader()
 
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
-;
+// uncomment for legacy applications that require $/jQuery as a global variable
+// .autoProvidejQuery()
 
 module.exports = Encore.getWebpackConfig();
