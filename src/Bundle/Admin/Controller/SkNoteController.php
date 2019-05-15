d89a34dce7bab9d8343de928086e23ea4dee648f
julkwel <julienrajerison5@gmail.com>
2019-05-15 03:04:32 +0300

    (fix) : note and matiere

--- src/Bundle/Admin/Controller/SkNoteController.php
+++ src/Bundle/Admin/Controller/SkNoteController.php
@@ -267,7 +267,7 @@ class SkNoteController extends Controller
         if (true === $_delete_note) {
             $this->getEntityService()->setFlash('success', 'Suppression du note effectuée');
 
-            return $this->redirectToRoute($request->getUri());
+            return $this->redirectToRoute('etudiant_note');
         }
     }
 
