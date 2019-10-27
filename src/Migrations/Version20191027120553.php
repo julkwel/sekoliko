d85c603053df16a2bac48a89bcbf4c798586067f
jrajerison <julienrajerison5@gmail.com>
2019-10-27 18:52:50 +0300

    (exception) : return false if entity not saved

---
+++ src/Migrations/Version20191027120553.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191027120553 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE administrator ADD school_year_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE administrator ADD CONSTRAINT FK_58DF0651D2EECC3F FOREIGN KEY (school_year_id) REFERENCES school_year (id)');
+        $this->addSql('CREATE INDEX IDX_58DF0651D2EECC3F ON administrator (school_year_id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE administrator DROP FOREIGN KEY FK_58DF0651D2EECC3F');
+        $this->addSql('DROP INDEX IDX_58DF0651D2EECC3F ON administrator');
+        $this->addSql('ALTER TABLE administrator DROP school_year_id');
+    }
+}
