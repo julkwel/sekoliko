6e313e0f97d739ada9a546a829e58393a7e8bb33
jrajerison <julienrajerison5@gmail.com>
2019-11-21 21:04:36 +0300

    (wip) : init student entity

---
+++ src/Repository/StudentRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\ClassRoom;
+use App\Entity\Student;
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method Student|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Student|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Student[]    findAll()
+ * @method Student[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class StudentRepository extends ServiceEntityRepository
+{
+    /**
+     * StudentRepository constructor.
+     *
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Student::class);
+    }
+
+    /**
+     * @param User      $user
+     * @param ClassRoom $classRoom
+     *
+     * @return Student[] Returns an array of Student objects
+     */
+    public function findByClassSchoolYearField(User $user, ClassRoom $classRoom)
+    {
+        return $this->createQueryBuilder('s')
+            ->where('s.deletedAt is NULL')
+            ->andWhere('s.etsName = :etsName')
+            ->andWhere('s.classe = :classRoom')
+            ->setParameter('etsName', $user->getEtsName())
+            ->setParameter('classRoom', $classRoom)
+            ->getQuery()
+            ->getResult();
+    }
+}
