f4213bd64e74f2a8de810e97af76a37026a75691
jrajerison <julienrajerison5@gmail.com>
2019-11-23 17:33:11 +0300

    (wip) : login page design

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addEntry('login', './assets/js/login.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
