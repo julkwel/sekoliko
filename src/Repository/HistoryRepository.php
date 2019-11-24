8799f79774d8547db6d3aac6d54c88e03f99ead5
jrajerison <julienrajerison5@gmail.com>
2019-11-24 19:57:52 +0300

    (wip) : add log stundent (init)

---
+++ src/Repository/HistoryRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\History;
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * @method History|null find($id, $lockMode = null, $lockVersion = null)
+ * @method History|null findOneBy(array $criteria, array $orderBy = null)
+ * @method History[]    findAll()
+ * @method History[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class HistoryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, History::class);
+    }
+
+    /**
+     * @param User $user
+     *
+     * @return History[] Returns an array of History objects
+     */
+    public function userHistory(User $user)
+    {
+        return $this->createQueryBuilder('h')
+            ->andWhere('h.user = :val')
+            ->setParameter('val', $user)
+            ->orderBy('h.id', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
