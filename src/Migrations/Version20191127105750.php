57623a19a37f6463e7cbce948dc7812004a22d4d
jrajerison <julienrajerison5@gmail.com>
2019-11-27 14:57:46 +0300

    (Entity) : Administration field update

---
+++ src/Migrations/Version20191127105750.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191127105750 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE scolarite CHANGE lieu_cin lieu_cin VARCHAR(50) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD sexe VARCHAR(10) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE scolarite CHANGE lieu_cin lieu_cin VARCHAR(50) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE user DROP sexe');
+    }
+}
---
+++ src/Migrations/Version20191127110629.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191127110629 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE administrator ADD diplome VARCHAR(100) DEFAULT NULL, ADD cin VARCHAR(50) DEFAULT NULL, ADD date_cin DATETIME DEFAULT NULL, ADD lieu_cin VARCHAR(50) DEFAULT NULL, ADD num_ae VARCHAR(150) DEFAULT NULL, ADD note_libre VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE administrator DROP diplome, DROP cin, DROP date_cin, DROP lieu_cin, DROP num_ae, DROP note_libre');
+    }
+}
