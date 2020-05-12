c000fe7606b8a969d2ee8469fa90fa1cd9dca454
julkwel <julienrajerison5@gmail.com>
2020-05-12 17:44:24 +0300

    fix circleCi

--- tests/Security/LoginTest.php
+++ tests/Security/LoginTest.php
@@ -34,6 +34,6 @@ class LoginTest extends WebTestCase
      */
     public function urlProvider(): ?Generator
     {
-        yield ['/{_locale}/login', 'GET'];
+        yield ['/login', 'GET'];
     }
 }
