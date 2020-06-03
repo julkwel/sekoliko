9d83005af52963aa67e66251aa5ca79a978f2d66
julkwel <julienrajerison5@gmail.com>
2020-06-03 22:46:09 +0300

    [FRONT] : make contact page work

--- assets/theme/front/assets/vendor/php-email-form/validate.js
+++ assets/theme/front/assets/vendor/php-email-form/validate.js
@@ -1,126 +1,121 @@
-jQuery(document).ready(function($) {
-  "use strict";
+import Axios from "axios";
+import swal from 'sweetalert2';
 
-  //Contact
-  $('form.php-email-form').submit(function() {
-   
-    var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+jQuery(document).ready(function ($) {
+    "use strict";
+    //Contact
+    $('#send-email').on('click', function (evt) {
+        evt.preventDefault();
+        var url = $(this).data('url');
+        var f = $(this).closest('form').find('.form-group'),
+            ferror = false,
+            emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
-     
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
+        f.children('input').each(function () {
+            var i = $(this); // current input
+            var rule = i.attr('data-rule');
 
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
+            if (rule !== undefined) {
+                var ierror = false; // error flag for current input
+                var pos = rule.indexOf(':', 0);
+                if (pos >= 0) {
+                    var exp = rule.substr(pos + 1, rule.length);
+                    rule = rule.substr(0, pos);
+                } else {
+                    rule = rule.substr(pos + 1, rule.length);
+                }
+
+                switch (rule) {
+                    case 'required':
+                        if (i.val() === '') {
+                            ferror = ierror = true;
+                        }
+                        break;
+
+                    case 'minlen':
+                        if (i.val().length < parseInt(exp)) {
+                            ferror = ierror = true;
+                        }
+                        break;
+
+                    case 'email':
+                        if (!emailExp.test(i.val())) {
+                            ferror = ierror = true;
+                        }
+                        break;
+
+                    case 'checked':
+                        if (!i.is(':checked')) {
+                            ferror = ierror = true;
+                        }
+                        break;
+
+                    case 'regexp':
+                        exp = new RegExp(exp);
+                        if (!exp.test(i.val())) {
+                            ferror = ierror = true;
+                        }
+                        break;
+                }
+                i.next('.validate').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+            }
+        });
+        f.children('textarea').each(function () {
+            var i = $(this); // current input
+            var rule = i.attr('data-rule');
+
+            if (rule !== undefined) {
+                var ierror = false; // error flag for current input
+                var pos = rule.indexOf(':', 0);
+                if (pos >= 0) {
+                    var exp = rule.substr(pos + 1, rule.length);
+                    rule = rule.substr(0, pos);
+                } else {
+                    rule = rule.substr(pos + 1, rule.length);
+                }
+
+                switch (rule) {
+                    case 'required':
+                        if (i.val() === '') {
+                            ferror = ierror = true;
+                        }
+                        break;
+
+                    case 'minlen':
+                        if (i.val().length < parseInt(exp)) {
+                            ferror = ierror = true;
+                        }
+                        break;
+                }
+                i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+            }
+        });
+        if (ferror) {
+            return false;
         } else {
-          rule = rule.substr(pos + 1, rule.length);
+            let name = $('#name').val();
+            let email = $('#email').val();
+            let subject = $('#subject').val();
+            let message = $('#message').val();
+
+            Axios.post(url, {
+                message: message,
+                email: email,
+                subject: subject,
+                name: name
+            }).then(res => {
+                if (res.data.status === 'success') {
+                    swal.fire('Sekoliko', 'Nous avons réçu votre message', 'success').then();
+                } else {
+                    swal.fire('Sekoliko', 'Une erreur c\'est produite !', 'error').then();
+                }
+            }).catch(err => {
+                console.log(err);
+                swal.fire('Sekoliko', 'Une erreur c\'est produite !', 'error').then();
+            })
         }
 
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'email':
-            if (!emailExp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'checked':
-            if (! i.is(':checked')) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'regexp':
-            exp = new RegExp(exp);
-            if (!exp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validate').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
+        return false;
     });
-    f.children('textarea').each(function() { // run all inputs
-
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
-
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
-    });
-    if (ferror) return false;
-    else var str = $(this).serialize();
-
-    var this_form = $(this);
-    var action = $(this).attr('action');
-
-    if( ! action ) {
-      this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html('The form action property is not set!');
-      return false;
-    }
-    
-    this_form.find('.sent-message').slideUp();
-    this_form.find('.error-message').slideUp();
-    this_form.find('.loading').slideDown();
-    
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        if (msg == 'OK') {
-          this_form.find('.loading').slideUp();
-          this_form.find('.sent-message').slideDown();
-          this_form.find("input:not(input[type=submit]), textarea").val('');
-        } else {
-          this_form.find('.loading').slideUp();
-          this_form.find('.error-message').slideDown().html(msg);
-        }
-      }
-    });
-    return false;
-  });
 
 });
--- src/Controller/Front/FrontController.php
+++ src/Controller/Front/FrontController.php
@@ -6,7 +6,12 @@
 namespace App\Controller\Front;
 
 use App\Controller\AbstractBaseController;
+use Exception;
+use Swift_Mailer;
+use Swift_Message;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,5 +28,40 @@ class FrontController extends AbstractBaseController
     public function redirectToLogin(): Response
     {
         return $this->render('front/content/_index.html.twig');
+    }
+
+    /**
+     * @Route("/send_email", name="send_contact_email", methods={"POST","GET"}, options={"expose"=true})
+     *
+     * @param Request      $request
+     * @param Swift_Mailer $mailer
+     *
+     * @return JsonResponse
+     */
+    public function sendEmail(Request $request, Swift_Mailer $mailer)
+    {
+        $data = json_decode($request->getContent(), true);
+        $name = $data['name'] ?? 'Sekoliko';
+        $email = $data['email'] ?? 'sekoliko.mada@gmail.com';
+        $subject = $data['subject'] ?? 'No subject';
+        $message = $data['message'] ?? 'No message';
+
+        $res = ['status' => 'error'];
+
+        try {
+            $swMessage = (new Swift_Message())
+                ->setSubject($subject)
+                ->setFrom($email, $name)
+                ->setCc('julienrajerison5@gmail.com')
+                ->setTo($this->getParameter('default_email'))
+                ->setBody($message, 'text/plain');
+
+            $mailer->send($swMessage);
+            $res = ['status' => 'success'];
+        } catch (Exception $exception) {
+            $res['reason'] = $exception->getMessage();
+        }
+
+        return $this->json($res);
     }
 }
