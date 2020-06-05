e34fb9762e7b0b71c635a222067248d69057b426
julkwel <julienrajerison5@gmail.com>
2020-06-05 08:45:56 +0300

    [GN] : add fullscreen.js

---
+++ assets/js/fullscreen.js
@@ -0,0 +1,22 @@
+/**
+ * Toggle fullscreen of page
+ *
+ * @param {string} id | the id will to toggle
+ */
+export default function openFullscreen(id) {
+    let element = document.getElementById('element');
+
+    if (element && !document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) { /* Firefox */
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) { /* IE/Edge */
+            element.msRequestFullscreen();
+        }
+    } else {
+        alert('Fullscreen not available on this browser !')
+    }
+}
